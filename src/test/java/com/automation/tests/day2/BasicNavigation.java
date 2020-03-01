991ea74f5a18d993ce2b411fd529a909df8846e9
dariaOzer <deryaozer23@gmail.com>
2020-03-01 15:59:49 -0500

    GetTitle() method and test

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -28,6 +28,7 @@ public class BasicNavigation {
         //ChromeDriver extends RemoteWebDriver
 
         driver.get("http://google.com"); //to open the website. We always start with get method.
+        //driver.manage().window().maximize(); // to maximize the browser
         Thread.sleep(3000); //  we did this for demo, wait 3 seconds
         //method that returns page title
         //you can also see it as tab name in browser
@@ -39,8 +40,42 @@ public class BasicNavigation {
         }else{
             System.out.println("TEST FAILED !");
         }
+        driver.navigate().to("https://amazon.com/"); // to navigate to another website
+        Thread.sleep(3000);
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("Test Passed---> Connecting to Amazon");
+        }else {
+            System.out.println("Test failed--> No Amazon Connection");
+        }
+
+        driver.navigate().back(); //To navigate back
+        Thread.sleep(3000);
+        verifyEquals(driver.getTitle(), "Google");
+
+        //move forward in the browser history
+        driver.navigate().forward();
+        Thread.sleep(3000);
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+        //to get tittle we use getTitle(); --> it returns A String
+
+        //to get URL
+        System.out.println("URL : "+ driver.getCurrentUrl()); // getCurrentUrl returns string
+
+        driver.navigate().refresh(); // to reload page
+        Thread.sleep(3000); //for demo wait 3 secs.
+
 
         driver.close(); //to close the browser
         //browser cannot close itself--> we need to call .close(); method.
+
+
+    }
+
+    public static void verifyEquals (String arg1, String arg2){
+          if(arg1.equals(arg2)){
+              System.out.println("Test passed");
+          }else{
+              System.out.println("test failed");
+          }
     }
 }
